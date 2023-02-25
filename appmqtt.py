795f8336ea128f2769a89a767c1e47bfffd469af
tienvinhle <tienvinh.industrial.automation@gmail.com>
2023-02-26 01:01:13 +0700

    update clientID, orgID to read from config file

--- appmqtt.py
+++ appmqtt.py
@@ -6,33 +6,35 @@ from collections import OrderedDict
 
 configPath = "/etc/mqtt/appConfig.conf"
 #orgID will be read from central config deliveried from Agent via Redis GET
-orgID = "mrViet"
-#device_id will be read from central config deliveried from Agent via Redis GET
-device_id = '61640dd128652f4a31tksfca'
-client_id = device_id
+#clientID and orgID will be read from central config deliveried from Agent via Redis GET
 
 def getConfig(configPath):
-	with open(configPath, 'r') as f:
-			config = json.load(f, object_pairs_hook=OrderedDict)
-			return config
+    try:
+        with open(configPath, 'r') as f:
+            config = json.load(f, object_pairs_hook=OrderedDict)
+    except ValueError:
+        config = None
+        print('No config file found or The file is invalid. Please check')
+    return config
 
 async def main():
     conf = getConfig(configPath)
-    mqtt = mqttClient.MQTTClient(client_id, orgID, conf["mqtt"]["user"], conf["mqtt"]["password"], int(conf["mqtt"]["QoS"]))
-    mqtt.connect(conf["mqtt"]["ip"], int(conf["mqtt"]["port"]), conf["mqtt"]["keepAliveTime"])
-    mqtt.client.loop_start()
-
-    rd = Message(conf["micro"]["ip"], int(conf["micro"]["port"]))
-    await rd.connect_to_redis()
-    rd.add_worker(mqtt)
-    format2Sub = conf["micro"]["subscription"]["format"]
-    data2Sub = conf["micro"]["subscription"]["data2Sub"]
-    tempChannel = None
-    for item in data2Sub:
-        tempChannel = format2Sub.replace("<thingID>", item["thingID"])
-        tempChannel = tempChannel.replace("<datapoint>", item["datapoint"])
-        print('Redis Client about to subsribe to {} channel', tempChannel)
-        await rd.add_channel(tempChannel)
+    if conf is not None:
+        mqtt = mqttClient.MQTTClient(conf["mqtt"]["clientID"], conf["mqtt"]["orgID"], conf["mqtt"]["user"], conf["mqtt"]["password"], int(conf["mqtt"]["QoS"]))
+        mqtt.connect(conf["mqtt"]["ip"], int(conf["mqtt"]["port"]), conf["mqtt"]["keepAliveTime"])
+        mqtt.client.loop_start()
+        
+        rd = Message(conf["micro"]["ip"], int(conf["micro"]["port"]))
+        await rd.connect_to_redis()
+        rd.add_worker(mqtt)
+        format2Sub = conf["micro"]["subscription"]["format"]
+        data2Sub = conf["micro"]["subscription"]["data2Sub"]
+        tempChannel = None
+        for item in data2Sub:
+            tempChannel = format2Sub.replace("<thingID>", item["thingID"])
+            tempChannel = tempChannel.replace("<datapoint>", item["datapoint"])
+            print('Redis Client about to subsribe to {} channel', tempChannel)
+            await rd.add_channel(tempChannel)
 
 if __name__ == '__main__':
     asyncio.run(main())
